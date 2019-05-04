2674f0c92c5007e0ac219ae0e8fdf91b648d0b18
Jack <mrzzcn@gmail.com>
2019-05-04 14:07:09 +0800

    :sparkles: feat: babel-loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 console.log('Hello Webpack', process.env.NODE_ENV);
+
+const arr = [1, 2, 3];
+const iAmJavascriptES6 = () => console.log(...arr);
+window.iAmJavascriptES6 = iAmJavascriptES6;
+
+console.log(iAmJavascriptES6());
